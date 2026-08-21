bf7e41e85394c0b1f99605c002ecb9b46dc338cc
mailovesara555-spec <mailovesara555@gmail.com>
2026-08-21 20:08:12 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/auction', (req, res) => {
 // รายชื่อ Discord Username ที่อนุญาตให้เข้าถึง
 // (ใช้ชื่อ username ปัจจุบันของ Discord ที่ไม่มี # ตัวเลขแล้ว เช่น 'admin_user')
 const ALLOWED_USERS = [
-    'admin_user',
+    'admin_user','daffodil2693',
     'guild_leader',
     'player123'
 ];
